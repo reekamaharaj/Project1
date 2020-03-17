090460c0b4a71b6bd597e8c00f63903a71eca7d5
Reeka <reekamaharaj@gmail.com>
2020-03-16 19:52:47 -0500

    food data API is working, created a list to help with keeping track of what needs to be done

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,79 +1,112 @@
 
 //variables
-let dietType = "";
-let recipeUrl = "";
+let dietType = "vegan";
 let number = 1;
 let apiKey = "633d97f5523a4f86a9b9fc20e109b345";
-let ingredients = "";
+let ingredients = "carrot";
+let idNum = "1051408"
+
+//get recipes
+function getRecipesById(){
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://api.spoonacular.com/recipes/" + idNum + "/information?apiKey=" + apiKey,
+        "method": "GET",
+        "headers": {
+        "content-type": "application/json"
+        }
+    }
+    
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    });
+}
 
 //search recipes
-var settings = {
-    "async": true,
-    "crossDomain": true,
-    "url": "https://api.spoonacular.com/recipes/search?number=5&diet=" + dietType + "&apiKey=" + apiKey,
-    "method": "GET",
-    "headers": {
-    "content-type": "application/json"
+function searchRecipe() {
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://api.spoonacular.com/recipes/search?number=5&diet=" + dietType + "&apiKey=" + apiKey,
+        "method": "GET",
+        "headers": {
+        "content-type": "application/json"
+        }
     }
-}
-$.ajax(settings).done(function (response) {
-    console.log(response);
-});
-
-//extract recipes
-var settings = {
-    "async": true,
-    "crossDomain": true,
-    "url": "https://api.spoonacular.com/recipes/extract?apiKey=" + apiKey + "&url="+ recipeUrl,
-    "method": "GET",
-    "headers": {
-    "content-type": "application/json"
-    }
-}
-$.ajax(settings).done(function (response) {
-    console.log(response);
-});
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    });
+};
 
 // random recipe
-var settings = {
-    "async": true,
-    "crossDomain": true,
-    "url": "https://api.spoonacular.com/recipes/random?number=" + number + "&apiKey=" + apiKey,
-    "method": "GET",
-    "headers": {
-    "content-type": "application/json"
+function randomRecipe(){
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://api.spoonacular.com/recipes/random?number=" + number + "&apiKey=" + apiKey,
+        "method": "GET",
+        "headers": {
+        "content-type": "application/json"
+        }
     }
-}
 
-$.ajax(settings).done(function (response) {
-    console.log(response);
-});
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    });
+};
 
 //generate meal plan
-var settings = {
-    "async": true,
-    "crossDomain": true,
-    "url": "https://api.spoonacular.com/mealplanner/generate?apiKey=" + apiKey + "&diet=" + dietType,
-    "method": "GET",
-    "headers": {
-    "content-type": "application/json"
+function generateMealPlan(){
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://api.spoonacular.com/mealplanner/generate?apiKey=" + apiKey + "&diet=" + dietType,
+        "method": "GET",
+        "headers": {
+        "content-type": "application/json"
+        }
     }
-}
-$.ajax(settings).done(function (response) {
-    console.log(response);
-});
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    });
+};
 
 //search recipes by ingredients
 //adding more ingredients will go in the url search "&ingredients=" + ingredient
-var settings = {
-    "async": true,
-    "crossDomain": true,
-    "url": "https://api.spoonacular.com/recipes/findByIngredients?apiKey=" + apiKey + "&ingredients=" + ingredients + "&number=" + number,
-    "method": "GET",
-    "headers": {
-    "content-type": "application/json"
+function recipesByIngredients(){
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://api.spoonacular.com/recipes/findByIngredients?apiKey=" + apiKey + "&ingredients=" + ingredients + "&number=" + number,
+        "method": "GET",
+        "headers": {
+        "content-type": "application/json"
+        }
     }
-}
-$.ajax(settings).done(function (response) {
-    console.log(response);
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    });
+};
+
+$("#random-button").on("click", function(event){
+    randomRecipe();
 });
+
+// will extract a recipe if a url is provided
+//extract recipes
+// let recipeUrl = "";
+// function extractRecipes(){
+//     var settings = {
+//         "async": true,
+//         "crossDomain": true,
+//         "url": "https://api.spoonacular.com/recipes/extract?apiKey=" + apiKey + "&url="+ recipeUrl,
+//         "method": "GET",
+//         "headers": {
+//         "content-type": "application/json"
+//         }
+//     }
+//     $.ajax(settings).done(function (response) {
+//         console.log(response);
+//     });
+// };

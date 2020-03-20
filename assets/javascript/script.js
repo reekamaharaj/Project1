18dc4f4405c23eb491771a8293df1c48d4bfe987
reekamaharaj <reekamaharaj@gmail.com>
2020-03-20 02:06:28 -0500

    on click of search button input vals will be saved and data will be searched
    All check boxes, text inputs values will be recored and search when the search button is clicked. Values will be reset to default when search is clicked. Default value for the search box is noodles, but this can be changed if the user wants. (The search being used needs a item to go into search)
    
    Have not been able to test to make sure this works yet, will check later.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,27 @@
+$(document).ready(function(){
+    randomRecipe();
+});
+
+//favRecipes is a function that will be called when the fav. page is loaded (html body tag calls this function) This is set up to create a recipe card with a recipe idNum. 
+function favRecipes(){
+    //saved recipes id numbers will be pulled from firebase 
+    //and below will create the recipes saved
+    var queryURL ="https://api.spoonacular.com/recipes/informationBulk?ids=" + idNum + "&apiKey=" + apiKey;
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response);
+        $("#title").html(response.title);
+        $("#recipe-image").html(response.image);
+        $("#ready-time").html(response.readyInMinutes);
+        $("#health-score").html(response.healthScore);
+        $("#summary").html(response.summary);
+        $("#fullRecipe").html("<a href='" + response.sourceUrl  + "' class='btn btn-primary' id='fullRecipe'>View Full Recipe</a>");
+        createRecipeCard();
+    });
+}
+
 // on click of the button to make a shopping list. A modal will popup unless the screen is too small, and then it will open a new tab. uses whisk API, allows user to login to a whisk account if they want to
 $("#list").on("click", function(event){
     whisk.queue.push(function() {
@@ -35,23 +59,30 @@ firebase.initializeApp(firebaseConfig);
 var data = firebase.database();
 
 //variables
-var dietType = [ ];
-var number = 1;
+//for search query
+var number = 2;
 var apiKey = "633d97f5523a4f86a9b9fc20e109b345";
-var ingredient = "carrot";
-var idNum = "1051408";
-var ingredients = [ ];
-var title;
-var image;
-var search;
+var idNum;
+var ingredientsEntered = [ ];
+var ingredients;
+var query = "noodles";
+var dietType;
+
+//for recipe card generation
+var recipeCard;
+var recipeImg = "";
+var title = "";
+var readyInMinutes = "";
+var healthScore = "";
+var summary = "";
+var sourceUrl = "";
 
 //user needs to provide a search query, then has the option to add ingredients or a diet type they would like the recipes to follow. On click of the search button, an array will be made for the ingredients (or we could just turn it into a string, the new search needs multiple params in a string separated by a ,) and will be made for the diet types. search will be run
 $("#search-button").on("click", function() {
     event.preventDefault();
 
-    search=$("#search-box").val().trim();
+    query = $("#search-box").val().trim();
     $("#search-box").val("");
-
 
     var ingredientOne = $("#ingredient-input").val().trim();
     $("#ingredient-input").val("");
@@ -68,26 +99,19 @@ $("#search-button").on("click", function() {
     var ingredientFive = $("#ingredient5-input").val().trim();
     $("#ingredient5-input").val("");
 
-    var query = $("#search-box").val().trim();
-    $("#search-box").val("");
+    $("#ingredient-input input[type=text]")
 
-    var glutenfree = $("#gluten-free-toggle").val();
-    $("#glutenfree").val("");
+    dietType = [ ];
+    $.each($("input[name='dietType']:checked"),
+    function (){
+        dietType.push($(this).val());
+    });
 
-    var ketogenic = $("#ketogenic-toggle").val();
-    $("#ketogenic").val("");
+    $("input[name='dietType").prop("checked", false);
 
-    var vegetarian = $("#vegetarian-toggle").val();
-    $("#vegetarian").val("");
+    dietType.join(",");
 
-    var vegan = $("#vegan-toggle").val();
-    $("#vegan").val("");
-
-    var paleo = $("#paleo-toggle").val();
-    $("#paleo").val("");
-
-    ingredients.push(
-        search,
+    ingredientsEntered.push(
         ingredientOne,
         ingredientTwo,
         ingredientThree,
@@ -95,18 +119,18 @@ $("#search-button").on("click", function() {
         ingredientFive
     );
 
-    //array into a string separated by comma
-    ingredients.join();
-    dietType.push(
-        glutenfree,
-        ketogenic,
-        vegetarian,
-        vegan,
-        paleo
-    );
+    ingredients = ingredientsEntered.filter(function(ingredient){
+        if (ingredient == ""){
+            return false;
+        }
+        else {
+            return true;
+        }
+    });
+    console.log(ingredients, dietType, query);
 
     //array into a string separated by comma
-    dietType.join();
+    ingredients.join(",");
 
     //search for recipes
     searchComplex();
@@ -117,69 +141,36 @@ $("#search-button").on("click", function() {
 //will search for recipes. Required params: query, apiKey. Optional dietType, ingredients. Number could be set to something, could give user the option to change it if we want to.
 //will need to change the query url depending on which params have values. If no diet was added we don't nee the "diet=" in the query url
 function searchComplex(){
-    var queryURL = "https://api.spoonacular.com/recipes/complexSearch?query=" + ingredients +  "2&apiKey=" + apiKey;
-
-    //var queryURL = "https://api.spoonacular.com/recipes/complexSearch?query=" + query + "&diet=" + dietType + "&includeIngredients=" + ingredients + "&iranking=2&number=" + number + "&apiKey=" + apiKey;
-
+    var queryURL = "https://api.spoonacular.com/recipes/complexSearch?query=" + query + "&dietType=" + dietType + "&includeIngredients=" + ingredients + "&number=" + number + "&apiKey=" + apiKey;
 
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function(response){
         console.log(response);
-    
-        $("#title").append(response.results[0].title);
-        $(".recipe-image").append(response.results[0].image);
-        $("#fullRecipe").append(response.results[0].sourceURL);
-      
-        $("#title2").append(response.results[1].title);
-        $(".recipe-image").append(response.results[1].image);
-        $("#fullRecipe2").append(response.results[1].sourceURL);
-      
-        $("#title3").append(response.results[2].title);
-        $(".recipe-image").append(response.results[2].image);
-        $("#fullRecipe3").append(response.results[2].sourceURL);
-        idNum = response.results[0].id;
-        getRecipesById();
-         
-       
-
-      
-  
-    
+        for (const i in response) {
+            idNum = response.results[i].id;
+            //get recipes
+            function getRecipesById() {
+                var queryURL ="https://api.spoonacular.com/recipes/informationBulk?ids=" + idNum + "&apiKey=" + apiKey;
+                $.ajax({
+                    url: queryURL,
+                    method: "GET"
+                }).then(function(response) {
+                    console.log(response);
+                    $("#title").html(response[i].title);
+                    $("#recipe-image").html(response[i].image);
+                    $("#ready-time").html(response[i].readyInMinutes);
+                    $("#health-score").html(response[i].healthScore);
+                    $("#summary").html(response[i].summary);
+                    $("#fullRecipe").html("<a href='" + response[i].sourceUrl  + "' class='btn btn-primary' id='fullRecipe'>View Full Recipe</a>");
+                    createRecipeCard();
+                    valueReset();
+                });
+            }
+        }
     });
-
-
-
-   
-   
 };
-
-//get recipes
-        $("#fullRecipe").append(response.results[0].sourceURL);
-        idNum = response.results[0].id;
-        getRecipesById();
-    });
-}
-
-function getRecipesById() {
-    var queryURL ="https://api.spoonacular.com/recipes/informationBulk?ids=" + idNum + "&apiKey=" + apiKey;
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response) {
-        console.log(response);
-        $("#ready-time").html(response[0].readyInMinutes);
-        $("#health-score").append(response[0].healthScore);
-        $("#fullRecipe").append(response[0].sourceUrl);
-        $("#summary").append(response[0].summary);
-        $("#fullRecipe").html("<a href='" + response[0].sourceUrl  + "' class='btn btn-primary' id='fullRecipe'>View Full Recipe</a>");
-
-    });
-    
-}
-
-
 
 // random recipe
 //will generate one random recipe and display it on the page
@@ -191,13 +182,32 @@ function randomRecipe() {
         method: "GET"
     }).then(function(response){
         console.log(response);
-        $("#title").append(response.recipes[0].title);
-        $(".recipe-image").append(response.recipes[0].image);
-        $("#health-score").append(response.recipes[0].healthScore);
-        $("#fullRecipe").html("<a href='" + response.recipes[0].sourceUrl  + "' class='btn btn-primary' id='fullRecipe'>View Full Recipe</a>");
-        $("#summary").append(response.recipes[0].summary);
-        
+        recipeImg = response.recipes[0].image;
+        title = response.recipes[0].title;
+        readyInMinutes = response.recipes[0].readyInMinutes;
+        healthScore = response.recipes[0].healthScore;
+        summary = response.recipes[0].summary;
+        sourceUrl = response.recipes[0].sourceUrl;
+        createRecipeCard();
+        valueReset();
     });
-    
 }
 
+function valueReset(){
+    ingredientsEntered = [ ];
+    ingredients = "";
+    dietType = "";
+    query = "noodles";
+    recipeImg = "";
+    title = "";
+    readyInMinutes = "";
+    healthScore = "";
+    summary = "";
+    sourceUrl = "";
+}
+
+function createRecipeCard(){
+    recipeCard = '<div class="card recipe-card"><div id="recipe-image"><img src="' + recipeImg + '"class="card-img-top" alt="Picture of Recipe"></div><div class="card-body"><h5 class="card-title" id="title">' + title + '</h5><p><i class="fas fa-clock" style="color: #f7941e;"></i> Ready in <span id="ready-time">' + readyInMinutes + '</span> minutes.</p><p>Health Score <span id="health-score">' + healthScore + '</span></p><p class="card-text" id="summary">' + summary + '</p><p id="fullRecipe"></p><a href="' + sourceUrl + '" class="btn add-to-favorites-button" data-toggle="tooltip" data-placement="right"title="Add to Favorites"><i class="far fa-star"></i></a></div>'
+    $("#recipeCards").append(recipeCard);
+    valueReset();
+}
